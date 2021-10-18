f991b15ddb8e8568a7194b0a7ed6cf19de5b32be
eliteSCHW31N <business@eliteschw31n.de>
2021-10-18 19:59:14 +0200

    update: add held Function

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ buttonpin = 21
 
 ledValue = 0.0
 buttonPressed = False
+fadeUp = False
 
 button = Button(buttonpin, hold_time=0.25)
 led = PWMLED(ledpin)
@@ -29,7 +30,22 @@ def toggleLed():
 
 
 def handleButtonHeld():
-    print('Held!')
+    global fadeUp
+    global ledValue
+    global buttonPressed
+    global button
+
+    while button.is_held:
+        if fadeUp:
+            if ledValue == 1.0:
+                fadeUp = False
+                return
+            ledValue = +0.1
+        else:
+            if ledValue == 0.0:
+                fadeUp = False
+                return
+            ledValue = -0.1
     return
 
 
