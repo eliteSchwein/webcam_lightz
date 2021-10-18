b046940ab10ed787f8e9f6761a8a6e04b0e0f458
eliteSCHW31N <business@eliteschw31n.de>
2021-10-18 19:11:03 +0200

    update: add held test

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from time import sleep
 ledpin = 18
 buttonpin = 21
 
+ledValue = 0.0
+buttonPressed = False
+
 button = Button(buttonpin)
 led = PWMLED(ledpin)
 
 if __name__ == '__main__':
     while True:
-        print('test')
+        led.value = ledValue
+
+        if(button.is_held):
+            print('Held!')
