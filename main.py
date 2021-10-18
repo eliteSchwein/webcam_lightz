1d3d0d5414e817a0972e934ba2de0c454d1a632a
eliteSCHW31N <business@eliteschw31n.de>
2021-10-18 19:03:45 +0200

    update: replace gpio lib

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from gpiozero import PWMLED, Button
+from time import sleep
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+ledpin = 18
+buttonpin = 21
 
+button = Button(buttonpin)
+led = PWMLED(ledpin)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        print('test')
